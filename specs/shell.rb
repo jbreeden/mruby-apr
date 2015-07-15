3dce2b2d031c2fe39838a8f2364afc9b1fccf43c
Jared Breeden <jared.breeden@gmail.com>
2015-07-14 19:12:37 -0700

    In progress... switching computers

--- specs/shell.rb
+++ specs/shell.rb
@@ -3,8 +3,14 @@ def via_cruby(cmd)
   File.open('sandbox/shell.rb', 'w') do |f|
     f.puts "system %q[#{cmd}]"
   end
-  pid = spawn "/usr/bin/ruby", 'sandbox/shell.rb'
+  if APR::OS == 'Windows'
+    pid = spawn "C:/Ruby22/bin/ruby.exe", 'sandbox/shell.rb'
+  else
+    pid = spawn "/usr/bin/ruby", 'sandbox/shell.rb'
+  end
   Process.wait(pid)
+rescue Exception => ex
+  puts ex
 end
 
 def as_program(cmd)
@@ -12,13 +18,21 @@ def as_program(cmd)
   err, pool = APR.apr_pool_create(nil)
   err, proc_attr = APR.apr_procattr_create pool
 
-  err = APR.apr_procattr_cmdtype_set proc_attr, APR::AprCmdtypeE::APR_PROGRAM_ENV
+  err = APR.apr_procattr_cmdtype_set proc_attr, APR::AprCmdtypeE::APR_PROGRAM_PATH
 
-  argv = ["/bin/sh", '-c', cmd]
+  argv = nil
+  if APR::OS == 'Windows'
+    argv = ["cmd.exe", '/C', cmd]
+  else
+    argv = ["/bin/sh", '-c', cmd]
+  end
   err, process = APR.apr_proc_create argv[0], argv, nil, proc_attr, pool
+  APR.raise_apr_errno(err)
 
   err, exitcode, exitwhy = APR.apr_proc_wait process, APR::AprWaitHowE::APR_WAIT
   exitcode == 0
+rescue Exception => ex
+  puts ex
 end
 
 def as_shellcmd_unified(cmd)
@@ -41,11 +55,15 @@ def as_shellcmd_unified(cmd)
     }
   end
 
-  #puts "Quoted argv: #{argv}"
+  puts "Quoted argv: #{argv}"
 
   err, process = APR.apr_proc_create argv[0], [cmd], nil, proc_attr, pool
+  APR.raise_apr_errno(err)
   err, exitcode, exitwhy = APR.apr_proc_wait process, APR::AprWaitHowE::APR_WAIT
   exitcode == 0
+
+rescue Exception => ex
+  puts ex
 end
 
 def as_shellcmd_tokenized(cmd)
@@ -68,17 +86,20 @@ def as_shellcmd_tokenized(cmd)
     }
   end
 
-  #puts "Quoted argv: #{argv}"
+  puts "Quoted argv: #{argv}"
 
   err, process = APR.apr_proc_create argv[0], argv, nil, proc_attr, pool
+  APR.raise_apr_errno(err)
   err, exitcode, exitwhy = APR.apr_proc_wait process, APR::AprWaitHowE::APR_WAIT
   exitcode == 0
+rescue Exception => ex
+  puts ex
 end
 
 def shell(cmd)
   via_cruby(cmd)
   as_program(cmd)
-  as_shellcmd_unified(cmd)
+  #as_shellcmd_unified(cmd)
   as_shellcmd_tokenized(cmd)
 end
 
@@ -93,10 +114,14 @@ end
 # See: http://rxr.whitequark.org/mri/source/win32/win32.c#099
 
 # Simple test
-test %q[echo 1]
+#test %q[echo 1]
 # Reving up with quotes
 test %q[ruby -e "puts(2)"]
 # Nesting quotes, escaped from the shell
 test %q[ruby -e "puts \"3\""]
 # Testing for shell interpretation of the command line
 test %q[ruby -r english -e "puts $INPUT_RECORD_SEPARATOR.length"]
+# Single quote in double quotes
+test %q[ruby -e "puts '3'.class"]
+# Double quotes in single quotes
+test %q[ruby -e 'puts "3".class']
