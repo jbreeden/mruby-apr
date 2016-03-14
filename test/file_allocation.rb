691b717159b7867c94afc36783f35fd23ce3b5c2
jbreeden <jared.breeden@gmail.com>
2016-03-13 20:12:25 -0700

    Remove unused test files

--- test/file_allocation.rb
+++
@@ -1,9 +0,0 @@
-files = []
-num_files = 200
-
-puts "Open Files"
-(1..num_files).each do |i|
-  files.push(File.open("temp/file#{i}", 'w'))
-end
-
-sleep 2
--- test/glob.rb
+++
@@ -1,3 +0,0 @@
-# mruby glob.rb  "/./a/{}b/c{}/d{e}f/{g,h,}/{,i,j}/{k/l}/[m/n]//o///p"
-# Dir::GlobAST.new(ARGV[0]).each_segment { |segment| puts segment.map { |pattern| "/#{pattern}/(#{pattern.class})"}}
-puts Dir[ARGV[0]].length
